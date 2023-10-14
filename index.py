1388849bf862353c5742d414f6661d31f78bc12c
袁隆成 <756885686@qq.com>
2023-10-15 02:31:09 +0800

    添加vue3的路由配置

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import subprocess
 import os
 
 
-def run_command(num):
+# 构建vue3项目
+def construct_vue3():
     print("请输入目标文件夹绝对路径(拖入文件夹)，直接按下回车将默认在桌面生成一个新文件夹")
     path = input()
     fileName = "mg"
@@ -26,23 +27,120 @@ def run_command(num):
         name = f"{tName}({counter})"
         createPath = os.path.join(folder_path, name)
         counter += 1
-    if num == 1:
-        command = 'npm create vite@latest {} -- --template vue-ts'.format(name)
-    elif num == 2:
-        command = 'vue create {}'.format(name)
-    else:
-        command = 'create-react-app {}'.format(name)
+    command = 'npm create vite@latest {} -- --template vue-ts'.format(name)
     try:
         print("脚本执行中。。。请稍后")
         result = subprocess.run(command, shell=True, check=True)
         if result.returncode == 0:
-            print("命令执行成功")
+            print("项目构建成功")
             # 在这里执行另一个命令
-            another_command = "npm install"
-            print("开始执行")
-            subprocess.run(another_command, shell=True, check=True, cwd=name)
+            initialize = "npm install"
+            print("开始安装依赖")
+            res = subprocess.run(initialize, shell=True,
+                                 check=True, cwd=name)
+            if res.returncode == 0:
+                print("依赖安装成功")
+                os.chdir(createPath)
+                vueRouter = "npm install vue-router@next"
+                rs = subprocess.run(vueRouter, shell=True, check=True)
+                print("开始安装路由")
+                if rs.returncode == 0:
+                    print("路由安装成功")
+                    print("开始生成路由文件")
+                    router()
+                    print("开始生成仓库文件")
+                    print("开始修改配置文件")
+                    revise()
+                else:
+                    print("路由安装失败！")
+            else:
+                print("依赖安装失败！")
+
         else:
-            print("命令执行失败")
+            print("项目构建失败")
+    except subprocess.CalledProcessError as e:
+        print(e)
+    else:
+        input("按下回车键退出")
+
+
+# 构建vue2项目
+def construct_vue2():
+    print("请输入目标文件夹绝对路径(拖入文件夹)，直接按下回车将默认在桌面生成一个新文件夹")
+    path = input()
+    fileName = "mg"
+    name = input("请输入项目名：")
+    while name == "":
+        print("请输入项目名称！")
+        name = input("请输入项目名：")
+    tName = name
+    counter = 1
+    if path:
+        folder_path = path
+    else:
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        folder_path = os.path.join(desktop_path, fileName)
+    # 打开或创建指定的文件夹
+    os.makedirs(folder_path, exist_ok=True)
+    os.chdir(folder_path)
+    createPath = os.path.join(folder_path, name)
+    # 检查项目名是否已在文件夹中存在
+    while os.path.exists(createPath):
+        name = f"{tName}({counter})"
+        createPath = os.path.join(folder_path, name)
+        counter += 1
+    command = 'vue create {}'.format(name)
+    try:
+        print("脚本执行中。。。请稍后")
+        result = subprocess.run(command, shell=True, check=True)
+        if result.returncode == 0:
+            print("项目构建成功")
+            # 在这里执行另一个命令
+            initialize = "npm install"
+            print("开始安装依赖")
+            subprocess.run(initialize, shell=True, check=True, cwd=name)
+        else:
+            print("项目构建失败")
+    except subprocess.CalledProcessError as e:
+        print(e)
+    else:
+        input("按下回车键退出")
+
+
+# 构建react项目
+def construct_react():
+    print("请输入目标文件夹绝对路径(拖入文件夹)，直接按下回车将默认在桌面生成一个新文件夹")
+    path = input()
+    fileName = "mg"
+    name = input("请输入项目名：")
+    while name == "":
+        print("请输入项目名称！")
+        name = input("请输入项目名：")
+    tName = name
+    counter = 1
+    if path:
+        folder_path = path
+    else:
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        folder_path = os.path.join(desktop_path, fileName)
+    # 打开或创建指定的文件夹
+    os.makedirs(folder_path, exist_ok=True)
+    os.chdir(folder_path)
+    createPath = os.path.join(folder_path, name)
+    # 检查项目名是否已在文件夹中存在
+    while os.path.exists(createPath):
+        name = f"{tName}({counter})"
+        createPath = os.path.join(folder_path, name)
+        counter += 1
+    command = 'create-react-app {}'.format(name)
+    try:
+        print("脚本执行中。。。请稍后")
+        result = subprocess.run(command, shell=True, check=True)
+        if result.returncode == 0:
+            print("项目构建成功")
+            # 在这里执行另一个命令
+        else:
+            print("项目构建失败")
     except subprocess.CalledProcessError as e:
         print(e)
     else:
@@ -66,6 +164,72 @@ def run_pip():
         print(e)
 
 
+# 生成router文件夹
+def router():
+    router_path = os.path.join(os.getcwd(), "src", "router")
+    print(router_path)
+    os.makedirs(router_path, exist_ok=True)
+    os.chdir(router_path)
+    file_path = os.path.join(os.getcwd(), "index.ts")
+    with open(file_path, "a") as file:
+        content = """
+        import { createRouter, createWebHashHistory, RouterOptions, Router, RouteRecordRaw } from 'vue-router'
+
+        const routes: RouteRecordRaw[] = [
+        { path: '/', name: 'Home', component: () => import('@/views/Home.vue') },
+        { path: '/about', name: 'About', component: () => import('@/views/About.vue') },
+        ]
+
+        // RouterOptions是路由选项类型
+        const options: RouterOptions = {
+        history: createWebHashHistory(),
+        routes,
+        }
+
+        // Router是路由对象类型
+        const router: Router = createRouter(options)
+
+        export default router
+        """
+        file.write(content)
+    os.chdir("..")
+
+
+# 修改main.ts文件
+def revise():
+    try:
+        file_path = os.path.join(os.getcwd(), "main.ts")
+        # 读取文件内容并修改
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print('原始内容：', content)
+
+            # 修改文件内容为引入vue-router后的内容
+        new_content = """
+        import { createApp } from 'vue'
+        import router from '@/router' 
+        import App from './App.vue'
+
+        const app = createApp(App)
+        app.use(router)
+        app.mount('#app')
+        """
+        with open(file_path, 'w') as file:
+            file.write(new_content)
+
+        print('修改后的内容：', new_content)
+
+        # 重新读取文件内容，验证修改是否成功
+        with open(file_path, 'r') as file:
+            modified_content = file.read()
+            print('最终内容：', modified_content)
+    except FileNotFoundError:
+        print('文件未找到')
+
+    except Exception as e:
+        print('出现错误：', e)
+
+
 print("请选择需要启动的脚本：")
 print("1.生成vue3项目  2.生成vue2项目  3.生成react项目  4.pip  5.退出")
 a = int(input())
@@ -73,11 +237,11 @@ while a != 1 and a != 2 and a != 3 and a != 4 and a != 5:
     print("输入不合法，请输入1、2、3、4、5其中一个数字！")
     a = int(input())
 if a == 1:
-    run_command(1)
+    construct_vue3()
 elif a == 2:
-    run_command(2)
+    construct_vue2()
 elif a == 3:
-    run_command(3)
+    construct_react()
 elif a == 4:
     run_pip()
 else:
